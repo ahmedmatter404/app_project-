976ff6e137046d736e3f68769922f6ad637775b0
Samar-Salah-7 <126681951+Samar-Salah-7@users.noreply.github.com>
2024-04-17 00:55:47 +0200

    first_version

---
+++ FirstVersion/Project_Mobile/frist_pages/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:frist_pages/features/auth/registration/view/page/regisration_page.dart';
+
+
+void main() {
+  MaterialApp materialApp = const MaterialApp(
+    home:  RegistrationPage(),
+  );
+
+  runApp(materialApp);
+}
+  
